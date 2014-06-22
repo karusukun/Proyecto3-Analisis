d07b3c0e0da7df4c8708477a43ba4de0ab07a371
boko <karusukun@gmail.com>
2014-06-22 12:28:10 -0500

    faijbuk online

--- Facebook friends/FbSocialCheck/FbSocialCheck/View/MainView.Designer.cs
+++ Facebook friends/FbSocialCheck/FbSocialCheck/View/MainView.Designer.cs
@@ -31,12 +31,15 @@
             this.btn_signIn = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
             this.WebFbLoginBrowser = new System.Windows.Forms.WebBrowser();
+            this.list_users = new System.Windows.Forms.ListBox();
+            this.combo_filters = new System.Windows.Forms.ComboBox();
+            this.btn_filter = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // btn_signIn
             // 
-            this.btn_signIn.Location = new System.Drawing.Point(100, 391);
+            this.btn_signIn.Location = new System.Drawing.Point(76, 558);
             this.btn_signIn.Name = "btn_signIn";
             this.btn_signIn.Size = new System.Drawing.Size(75, 23);
             this.btn_signIn.TabIndex = 0;
@@ -49,7 +52,7 @@
             this.panel1.Controls.Add(this.WebFbLoginBrowser);
             this.panel1.Location = new System.Drawing.Point(12, 12);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(275, 360);
+            this.panel1.Size = new System.Drawing.Size(909, 530);
             this.panel1.TabIndex = 1;
             // 
             // WebFbLoginBrowser
@@ -58,14 +61,44 @@
             this.WebFbLoginBrowser.Location = new System.Drawing.Point(0, 0);
             this.WebFbLoginBrowser.MinimumSize = new System.Drawing.Size(20, 20);
             this.WebFbLoginBrowser.Name = "WebFbLoginBrowser";
-            this.WebFbLoginBrowser.Size = new System.Drawing.Size(275, 360);
+            this.WebFbLoginBrowser.Size = new System.Drawing.Size(909, 530);
             this.WebFbLoginBrowser.TabIndex = 0;
+            this.WebFbLoginBrowser.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.WebFbLoginBrowser_DocumentCompleted);
+            // 
+            // list_users
+            // 
+            this.list_users.FormattingEnabled = true;
+            this.list_users.Location = new System.Drawing.Point(961, 84);
+            this.list_users.Name = "list_users";
+            this.list_users.Size = new System.Drawing.Size(185, 472);
+            this.list_users.TabIndex = 2;
+            // 
+            // combo_filters
+            // 
+            this.combo_filters.FormattingEnabled = true;
+            this.combo_filters.Location = new System.Drawing.Point(961, 22);
+            this.combo_filters.Name = "combo_filters";
+            this.combo_filters.Size = new System.Drawing.Size(150, 21);
+            this.combo_filters.TabIndex = 3;
+            // 
+            // btn_filter
+            // 
+            this.btn_filter.Location = new System.Drawing.Point(997, 49);
+            this.btn_filter.Name = "btn_filter";
+            this.btn_filter.Size = new System.Drawing.Size(75, 23);
+            this.btn_filter.TabIndex = 4;
+            this.btn_filter.Text = "Filter";
+            this.btn_filter.UseVisualStyleBackColor = true;
+            this.btn_filter.Click += new System.EventHandler(this.btn_filter_Click);
             // 
             // MainView
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(732, 440);
+            this.ClientSize = new System.Drawing.Size(1158, 625);
+            this.Controls.Add(this.btn_filter);
+            this.Controls.Add(this.combo_filters);
+            this.Controls.Add(this.list_users);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.btn_signIn);
             this.Name = "MainView";
@@ -80,6 +113,9 @@
         private System.Windows.Forms.Button btn_signIn;
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.WebBrowser WebFbLoginBrowser;
+        private System.Windows.Forms.ListBox list_users;
+        private System.Windows.Forms.ComboBox combo_filters;
+        private System.Windows.Forms.Button btn_filter;
 
     }
 }
--- Facebook friends/FbSocialCheck/FbSocialCheck/View/MainView.cs
+++ Facebook friends/FbSocialCheck/FbSocialCheck/View/MainView.cs
@@ -8,22 +8,32 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using Facebook;
+using System.Threading;
 using System.Dynamic;
 
 namespace FbSocialCheck.View
 {
     public partial class MainView : Form
     {
-        private const string AppID = "1434678843460011";
-        private Uri _loginUrl;
-        private const string _extendedPermissions = "user_about_me,publish_stream,offline_access";
-        FacebookClient fbClient = new FacebookClient();
         public String access_token;
-
+        private DateTime fechaAct;
+        private Dictionary<string, int> filterOptionMultiplex;
 
         public MainView()
         {
+            filterOptionMultiplex = new Dictionary<string, int>();
+
+            filterOptionMultiplex.Add("Year",0);
+            filterOptionMultiplex.Add("Month", 1);
+            filterOptionMultiplex.Add("Week", 2);
+            
+            fechaAct = DateTime.Now;
             InitializeComponent();
+            
+            this.combo_filters.Items.Add("Year");
+            this.combo_filters.Items.Add("Month");
+            this.combo_filters.Items.Add("Week");
+            this.btn_filter.Enabled = false;
         }
 
     
@@ -31,14 +41,15 @@ namespace FbSocialCheck.View
         private void btn_signIn_Click(object sender, EventArgs e)
         {
             String OAuthURL = @"https://www.facebook.com/dialog/oauth?client_id=1434678843460011
-                              &redirect_uri = https://www.facebook.com/connect/login_success.html
+                               &redirect_uri=https://www.facebook.com/connect/login_success.html
                               &response_type=token
-                              &scope= user_groups,user_status";
+                              &scope=user_groups,user_status"; 
+                              
 
             this.WebFbLoginBrowser.Navigate(OAuthURL);
         }
 
-        private void WebFacebook_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        private void WebFbLoginBrowser_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
         {
             if (this.WebFbLoginBrowser.Url.AbsoluteUri.Contains("access_token"))
             {
@@ -54,13 +65,71 @@ namespace FbSocialCheck.View
                 string NAME = data.name;
                 string imgURL = string.Format("http://graph.facebook.com/{0}/picture&type=normal", ID);
 
-                MessageBox.Show("ID = " + data.id);
+                //MessageBox.Show("ID = " + data.id+ " Nombre "+ data.name);
 
+                
+                this.btn_filter.Enabled = true;
+               
             }
-            else{
-                MessageBox.Show("No shit is happening bro");
+            
+        }
+
+        private void evaluateList(int converCount, dynamic conversations, int option, string myName, int evaluator)
+        {
+            for (int position = 0; position < converCount; position++)
+            {
+                //MessageBox.Show("prueba, "+ conversations.data[0].updated_time );
+                string fecha = (string)conversations.data[position].updated_time;
+                string subFecha = fecha.Substring(0, fecha.IndexOf("T"));
+
+                string[] splitedDate = subFecha.Split('-');
+
+                //MessageBox.Show("mes: "+ splitedDate[1] );
+                if (int.Parse(splitedDate[option]) == evaluator && conversations.data[position] != null)
+                {
+                    string nameToAdd = conversations.data[position].participants.data[0].name;
+                    if (myName.Equals(nameToAdd))
+                        nameToAdd = conversations.data[position].participants.data[1].name;
+                    conversations.data[position] = null;
+                    this.list_users.Items.Add(nameToAdd);
+                }
             }
         }
 
+        private void btn_filter_Click(object sender, EventArgs e)
+        {
+            list_users.Items.Clear();   
+            int option = filterOptionMultiplex[combo_filters.Text];
+            FacebookClient fb = new FacebookClient(access_token);
+            dynamic conversations = fb.Get("me/conversations?fields=participants,updated_time");
+            dynamic myself = fb.Get("/me");
+            string myName = (string) myself.name;
+            int conversationCount = (int)conversations.data.Count;
+            int evaluator = 0;
+
+            switch (option)
+            { 
+                case 0:
+                    evaluator = fechaAct.Year;
+                    break;
+                case 1:
+                    evaluator = fechaAct.Month;
+                    break;
+                case 2:
+                    evaluator = fechaAct.Day;
+                    break;
+                default:
+                    evaluator = 0;
+                    break;
+            }
+            
+            evaluateList(conversationCount, conversations, option, myName, evaluator);
+                       
+
+        }
+
+
+
+       
     }
 }
